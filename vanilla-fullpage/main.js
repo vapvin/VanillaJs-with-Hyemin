3c99e99ad7b66490b3c8f33a0c44417873099de9
vapvin <vins@vins.dev>
2020-10-21 14:59:45 +0900

    Fuck Typing Error

---
+++ vanilla-fullpage/main.js
@@ -0,0 +1,48 @@
+const SHOW = 'show'
+let next;
+let prev;
+const scrollEffects = element => {  
+    window.scrollTo({
+      'behavior': 'smooth',
+      'left': 0,
+      'top': element.offsetTop
+    });
+  };
+  const scrolling = event => {
+    let tags = ['HEADER', 'SECTION', 'FOOTER'];
+
+    const {target} = event;
+    
+    if(tags.includes(target.nodeName)){
+       
+        next = target.nextElementSibling;
+        prev = target.previousElementSibling;
+    } else {
+        next = target.closest(tags).nextElementSibling;
+        prev = target.closest(tags).previousElementSibling;
+    }
+
+
+    if(event.deltaY < 0){
+        event.preventDefault()
+        const {target} = event;
+        if(tags.includes(prev.nodeName)){
+            scrollEffects(prev, 1800, 'easeInOutQuad');
+            target.closest(tags).classList.remove(SHOW);
+            prev.classList.add(SHOW);
+        }
+        
+    } else if(event.deltaY > 0){
+        event.preventDefault();
+        const {target} = event;
+        if(tags.includes(next.nodeName)){
+            scrollEffects(next, 1800, 'easeInOutQuad');
+            target.closest(tags).classList.remove(SHOW);
+            next.classList.add(SHOW);
+        }
+    }else{
+        return false;
+    }
+} 
+document.body.addEventListener('wheel', scrolling);
+
