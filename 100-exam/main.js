a4e3a19fce88f65214b26b8f0e821cab51a93fb4
vapvin <vins@vins.dev>
2020-10-30 11:35:11 +0900

    Push

---
+++ 100-exam/main.js
@@ -0,0 +1,7 @@
+const str = "Javascript";
+const arr = str.split("");
+console.log(arr);
+
+for (i = 0; i < arr.length - 1; i++) {
+  console.log(arr[i], arr[i + 1]);
+}
--- clock/main.js
+++ clock/main.js
@@ -1,17 +1,12 @@
 const DEGREE = 6;
-
 const hourpin = document.querySelector("#hr-pin");
 const minpin = document.querySelector("#min-pin");
 const secpin = document.querySelector("#seconds");
-
-
-
 const watch = () => {
     const day = new Date();
     const hour = day.getHours() * 30;
     const min = day.getMinutes() * DEGREE;
     const sec = day.getSeconds() * DEGREE;
-
     hourpin.style.transform = `rotate(${(hour) + (min/12)}deg)`;
     minpin.style.transform = `rotate(${min}deg)`;
     secpin.style.transform = `rotate(${sec}deg)`;
