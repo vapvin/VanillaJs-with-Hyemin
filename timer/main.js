2787cb23b5c773374398edcefdf665c69da5a00c
vapvin <vins@vins.dev>
2020-11-04 22:59:21 +0900

    Basic Usage

--- timer/main.js
+++ timer/main.js
@@ -1,3 +1,9 @@
 // setTimeout
 
-let timeoutName = setTimeout(func | code, [delay], [arg1], [arg2]);
+// let timeoutName = setTimeout(func | code, [delay], [arg1], [arg2]);
+
+function test(){
+    console.log("Test");
+}
+
+setTimeout(test, 100);
