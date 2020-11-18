795b84d5660780c74a887f8c1c6a7c7f3844086e
vapvin <vins@vins.dev>
2020-11-19 00:30:00 +0900

    Boiler Plate

---
+++ complete-todo/src/Item.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Item extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render (){
+        return (
+            <li></li>
+        )
+    }
+}
---
+++ complete-todo/src/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class List extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <ul></ul>
+        )
+    }
+}
