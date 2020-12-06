a68a6ff1f3c2391b4c24c5fc9a3392f09e513c91
vapvin <vins@vins.dev>
2020-12-06 19:20:53 +0900

    Basic Network

---
+++ go-microservice/http.go
@@ -0,0 +1,12 @@
+func main(){
+	port := 8080
+
+	http.HandleFunc("/helloworld", helloWorldHandler)
+
+	log.Printf("Server starting on port %v\n", 8080)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!\n")
+}
