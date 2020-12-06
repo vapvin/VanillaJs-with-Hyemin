4aceb5f1a23ba641afdc1135860d3e4eeabf0a4b
vapvin <vins@vins.dev>
2020-12-06 19:27:25 +0900

    Fetching Google functions

--- go-microservice/http.go
+++ go-microservice/http.go
@@ -24,3 +24,12 @@ func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	encoder.Encode(response)
 	
 }
+
+func fetchGoogle(t *testing.T){
+	r, _ := http.NewRequest("GET", "https://google.com", nil)
+
+	timeoutRequest, cancelFunc := context.WithTimeout(r.Context(), 1*time.Millisecond)
+	defer cancelFunc()
+
+	r = r.WithContext(timeoutRequest)
+}
