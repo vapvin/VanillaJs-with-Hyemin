bb10f2ae2de5d5ecd63f66545caf78472eb597a9
vapvin <tpehdj12@daum.net>
2021-01-02 15:39:46 +0900

    Arr Method: shift()

--- arr_method/main.js
+++ arr_method/main.js
@@ -11,6 +11,13 @@ arr2.push(6);
 console.log(arr2); // [1,2,3,4,5,6]
 
 // unshift() 배열의 첫 인덱스에 값을 삽입
+
 const arr3 = [2,3,4];
 arr3.unshift(1);
 console.log(arr3); // [1,2,3,4];
+
+// shift() 배열의 첫 인덱스의 값을 삭제
+
+const arr4 = [1,2,3,4];
+arr4.shift();
+console.log(arr4);
