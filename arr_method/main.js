affd1222a4afdb4f4f54ca542de88253d97abcd9
vapvin <tpehdj12@daum.net>
2021-01-02 15:36:39 +0900

    Arr Method: push()

--- arr_method/main.js
+++ arr_method/main.js
@@ -3,3 +3,9 @@
 const arr1 = [1, 2, 3, 4, 5];
 arr1.pop();
 console.log(arr1); // [1,2,3,4]
+
+// push() 배열의 마지막 인덱스에 값을 삽입
+
+const arr2 = [1, 2, 3, 4, 5];
+arr2.push(6);
+console.log(arr2); // [1,2,3,4,5,6]
