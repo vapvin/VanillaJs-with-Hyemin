09661abc3abc407dbbff5b51c60fc3b445fe252a
vapvin <tpehdj12@daum.net>
2021-01-18 23:39:50 +0900

    Global This

--- functions/object.js
+++ functions/object.js
@@ -55,19 +55,81 @@
 // console.log(obj4);
 
 
+// const obj = {
+//     name: "현규",
+//     sayHello: function(){
+//         console.log(this.name + "님 안녕하세요");
+//     }
+// }
+
+// obj.sayHello();
+
+// const a = "test";
+
+// function test(){
+//     console.log(this);
+// }
+
+// test();
+
+// 생성자 함수
+
+// function Person(name, age){ // 생성자 함수는 new 키워드로 호출을 하게되면 빈객체를 먼저 생성한다.
+//     this.name = name; // 함수 내부의 this는 처음 생성된 빈 객체를 가르키게 된다.
+//     this.age = age;
+//     console.log(this);
+// }
+
+// const person1 = Person("test", 12);
+
+// console.log(person1)
+
+// const Person = () => {
+//     this.age = 0;
+//     console.log(this.age);
+
+//     setTimeout(() => {
+//         this.age++;
+//         console.log(this.age);
+//     }, 1000);
+// }
+
+// Person();
+
+// const obj = {
+
+//     abc: () => {
+//         console.log(this);
+//         setTimeout(() => {
+//             console.log(this)
+//             setTimeout(() => {
+//                 console.log(this);
+//             }, 100)
+//         }, 100)
+//     }
+// }
+
+// obj.abc();
+
 const obj = {
-    name: "현규",
-    sayHello: function(){
-        console.log(this.name + "님 안녕하세요");
+    aaa:6,
+    acd(){
+        console.log(this);
+    },
+    abc: {
+        test:{
+            a: 5,
+            ad: () => {
+                console.log(this);
+            }
+        }
+    },
+    abcd: () => {
+        console.log(this);
     }
 }
 
-obj.sayHello();
+obj.abcd();
+obj.acd();
+obj.abc.test.ad();
 
-const a = "test";
-
-function test(){
-    console.log(this);
-}
-
-test();
