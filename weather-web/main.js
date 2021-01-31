c5c7c11256ccc6f8dccca6782f0460732dbc1c5b
vapvin <vins@vins.dev>
2021-02-01 00:40:23 +0900

    Weather Complete

--- weather-web/main.js
+++ weather-web/main.js
@@ -1,11 +1,24 @@
+const tempText = document.querySelector(".temp");
+const minTempText = document.querySelector(".min-temp");
+const maxTempText = document.querySelector(".max-temp");
+const descriptionText = document.querySelector(".description");
+const iconTag = document.querySelector(".icon");
+
 let lon;
 let lat;
 
 async function getWeather(lat, lon){
     console.log(lon, lat)
-    const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2bb4d7864e57694acbe21989fe494cba&lang=ko-KR`);
+    const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2bb4d7864e57694acbe21989fe494cba&lang=ko-KR&units=metric`);
     const result = await data.json();
-    console.log(result);
+    const {main: {temp, temp_max, temp_min}, weather: [weather]} = result;
+    const {main, icon} = weather;
+    console.log(temp, temp_max, temp_min, main, icon)
+    tempText.innerText = temp;
+    minTempText.innerText = temp_min;
+    maxTempText.innerText = temp_max;
+    descriptionText.innerText = main;
+    iconTag.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
 }
 
 function getLocation(){
